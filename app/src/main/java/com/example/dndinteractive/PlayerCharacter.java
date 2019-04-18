cc56cce517cf0602c88a2dbf3a50757a23f9cfb7
Karmatic Neutral <karmaticneutral@gmail.com>
2019-04-17 22:04:39 -0600

    fix Player Character.

--- app/src/main/java/com/example/dndinteractive/PlayerCharacter.java
+++ app/src/main/java/com/example/dndinteractive/PlayerCharacter.java
@@ -1,10 +1,54 @@
 package com.example.dndinteractive;
 
 import com.example.dndinteractive.Sheet.Sheet;
+import com.example.dndinteractive.creatureinfo.Location;
+import com.example.dndinteractive.creatureinfo.Size;
 
-public class PlayerCharacter implements LivingObject{
+public class PlayerCharacter implements LivingObject {
+    private String name;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public Sheet getSheet() {
         return null;
+    }
+
+    @Override
+    public void setSheet(Sheet sheet) {
+
+    }
+
+    @Override
+    public Location getLocation() {
+        return null;
+    }
+
+    @Override
+    public void setLocation(Location location) {
+
+    }
+
+    @Override
+    public void move(int xChange, int yChange) {
+
+    }
+
+    @Override
+    public Size getSize() {
+        return null;
+    }
+
+    @Override
+    public void setSize(Size size) {
+
     }
 }
